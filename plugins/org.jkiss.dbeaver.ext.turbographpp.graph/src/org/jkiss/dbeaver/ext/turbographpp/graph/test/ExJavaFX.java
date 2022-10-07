931bfe844a5bdb1d9d9462df822b7a298ef5a3c2
hwany7seo <hwanyseo@cubrid.com>
2022-10-07 13:46:28 +0900

    [GT-32] 시각화 창 - JAVAFX를 SWT 환경에 적용하기 위한 테스트 (#46)
    http://jira.iitp.cubrid.org/browse/GT-32
    
    - JAVAFX 기본 테스트
    - JavaFXSmartGraph Lib를 사용한 예제 테스트
    - Java-Graph-Algorithms-Visualizer Lib를 사용한 예제 테스트

---
+++ plugins/org.jkiss.dbeaver.ext.turbographpp.graph/src/org/jkiss/dbeaver/ext/turbographpp/graph/test/ExJavaFX.java
@@ -0,0 +1,54 @@
+package org.jkiss.dbeaver.ext.turbographpp.graph.test;
+
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+
+import javafx.animation.RotateTransition;
+import javafx.animation.ScaleTransition;
+import javafx.embed.swt.FXCanvas;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.util.Duration;
+
+public class ExJavaFX {
+    
+    private Button button;
+
+    public ExJavaFX(Composite parent, int style) {
+        FXCanvas canvas = new FXCanvas(parent, SWT.NONE);
+        GridDataFactory
+            .fillDefaults()
+            .grab(true, true)
+            .span(3, 1)
+            .applyTo(canvas);
+    
+        BorderPane layout = new BorderPane();
+    
+        Scene scene = new Scene(layout, Color.rgb(
+            parent.getShell().getBackground().getRed(),
+            parent.getShell().getBackground().getGreen(),
+            parent.getShell().getBackground().getBlue()));
+    
+        canvas.setScene(scene);
+        
+        javafx.scene.control.Label output = new javafx.scene.control.Label();
+        layout.setCenter(output);
+        
+        RotateTransition rotateTransition = 
+                new RotateTransition(Duration.seconds(1), output);
+        rotateTransition.setByAngle(360);
+
+        ScaleTransition scaleTransition = 
+                new ScaleTransition(Duration.seconds(1), output);
+        scaleTransition.setFromX(1.0);
+        scaleTransition.setFromY(1.0);
+        scaleTransition.setToX(4.0);
+        scaleTransition.setToY(4.0);
+        
+        button = new Button(parent, SWT.None);
+        button.setText("Test");
+    }
+}
