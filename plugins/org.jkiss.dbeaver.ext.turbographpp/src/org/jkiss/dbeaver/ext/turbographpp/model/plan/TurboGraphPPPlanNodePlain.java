f3b5864d52724d51e6cab03467133da4c5383153
hwany7seo <hwanyseo@cubrid.com>
2024-11-01 13:31:24 +0900

    [GT-178] PLAN 정보 추가 표시 (#115)
    http://jira.iitp.cubrid.org/browse/GT-178
    
    - TreeView에 Rows, Time, extra 정보 표시하도록 추가.
    - TextTab을 추가하여 FullText 정보 출력하도록 추가.
    - broker가 최신버전이 아니라 rows와 time정보가 없더라도 제외하고 정상적으로 출력 되도록 수정.

--- plugins/org.jkiss.dbeaver.ext.turbographpp/src/org/jkiss/dbeaver/ext/turbographpp/model/plan/TurboGraphPPPlanNodePlain.java
+++ plugins/org.jkiss.dbeaver.ext.turbographpp/src/org/jkiss/dbeaver/ext/turbographpp/model/plan/TurboGraphPPPlanNodePlain.java
@@ -1,39 +1,40 @@
 package org.jkiss.dbeaver.ext.turbographpp.model.plan;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import org.jkiss.code.Nullable;
-import org.jkiss.dbeaver.model.exec.plan.DBCPlanCostNode;
-import org.jkiss.dbeaver.model.impl.PropertyDescriptor;
+
+import org.jkiss.code.NotNull;
 import org.jkiss.dbeaver.model.impl.plan.AbstractExecutionPlanNode;
 import org.jkiss.dbeaver.model.meta.Property;
-import org.jkiss.dbeaver.model.preferences.DBPPropertyDescriptor;
-import org.jkiss.dbeaver.model.preferences.DBPPropertySource;
-import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
 
-public class TurboGraphPPPlanNodePlain extends AbstractExecutionPlanNode
-        implements DBCPlanCostNode, DBPPropertySource {
+public class TurboGraphPPPlanNodePlain extends AbstractExecutionPlanNode {
 
     private TurboGraphPPPlanNodePlain parent;
+    private String type;
     private String name;
-    private JsonObject object;
+    private String plan;
+    private int blankCount;
     private Map<String, String> nodeProps = new LinkedHashMap<>();
     private List<TurboGraphPPPlanNodePlain> nested = new ArrayList<>();
 
-    public TurboGraphPPPlanNodePlain(
-            TurboGraphPPPlanNodePlain parent, String name, JsonObject object) {
+    public TurboGraphPPPlanNodePlain(TurboGraphPPPlanNodePlain parent, String type, String plan) {
         this.parent = parent;
-        this.name = name;
-        this.object = object;
+        this.type = type;
+        this.name = "";
+        this.plan = plan;
 
-        parseObject(name, object);
+        try {
+            parsePlan(name);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public TurboGraphPPPlanNodePlain(
@@ -46,81 +47,129 @@ public class TurboGraphPPPlanNodePlain extends AbstractExecutionPlanNode
         return nodeProps;
     }
 
-    private void parseObject(String objName, JsonObject object) {
-        for (Map.Entry<String, JsonElement> prop : object.entrySet()) {
-            String propName = prop.getKey();
-            JsonElement value = prop.getValue();
-            if (value instanceof JsonPrimitive) {
-                if ("Node Type".equals(propName)) {
-                    this.name = String.valueOf(value);
-                    nodeProps.put(propName, value.toString());
-                }
-            } else if (value instanceof JsonArray) {
-                boolean isProp = false;
-                int itemIndex = 0;
-                for (JsonElement item : (JsonArray) value) {
-                    if (item instanceof JsonObject) {
-                        itemIndex++;
-                        addNested(propName + "#" + itemIndex, (JsonObject) item);
+    private void parsePlan(String objName) throws IOException {
+        BufferedReader reader = new BufferedReader(new StringReader(plan));
+        String subPlans = "";
+        List<String> parentPlans = new ArrayList<>();
+        boolean isFirst = true;
+        int lineCount = 0;
+        String line;
+
+        //        첫라인은 무조건 현재 nodeProp에 추가
+        //        두째라인부터는 묶어서 현재에 nested에 추가합니다.
+        //        같은 등급이 나오면 같은 등급끼리 묶어서 parent로 보냅니다.
+        while ((line = reader.readLine()) != null) {
+            if (isFirst) {
+                blankCount = getBlankCount(line);
+                addNodeProps(line);
+                isFirst = false;
+            } else {
+                lineCount = getBlankCount(line);
+                if (blankCount == lineCount) {
+                    parentPlans.add(line);
+                } else {
+                    int size = parentPlans.size();
+                    if (size > 0) {
+                        String plan = parentPlans.get(size - 1) + System.lineSeparator() + line;
+                        parentPlans.remove(size - 1);
+                        parentPlans.add(plan);
                     } else {
-                        isProp = true;
-                        break;
+                        subPlans = subPlans + line + System.lineSeparator();
                     }
                 }
-                if (isProp) {
-                    nodeProps.put(propName, value.toString());
-                }
-            } else {
-                nodeProps.put(propName, value.getAsString());
             }
+        }
+        if (!subPlans.isEmpty()) {
+            addThisNested("", subPlans);
+        }
+
+        Iterator<String> it = parentPlans.iterator();
+        while (it.hasNext()) {
+            addParentNested("", it.next());
         }
     }
 
-    private void addNested(String name, JsonObject value) {
+    @NotNull
+    private void addNodeProps(String subplan) {
+        if (subplan != null && !subplan.equals("") && subplan.indexOf("(") > 0) {
+            name = subplan.substring(0, subplan.indexOf("(")).trim();
+            String value = subplan.substring(subplan.indexOf("("), subplan.length());
+            value = value.replace("(", "");
+            String[] values = value.split("\\)");
+
+            String[] itemsValue = values[0].split(",");
+            
+            if (itemsValue.length != 2) {
+                nodeProps.put("extra", value.replace(")", ""));
+                type = type + name;
+                return;
+            }
+            
+            for (int i = 0; i < itemsValue.length; i++) {
+                String[] items = itemsValue[i].split(":");
+                if (items.length == 2) {
+                    nodeProps.put(items[0].trim(), items[1].trim());
+                }
+            }
+
+            String extra = "";
+            for (int i = 1; i < values.length; i++) {
+                extra = extra + values[i];
+            }
+            nodeProps.put("extra", extra);
+        } else {
+            name = subplan.trim();
+        }
+
+        type = type + name;
+    }
+
+    private void addThisNested(String name, String plan) {
         if (nested == null) {
             nested = new ArrayList<>();
         }
-        nested.add(new TurboGraphPPPlanNodePlain(this, name, value));
+        if (!plan.isEmpty()) {
+            nested.add(new TurboGraphPPPlanNodePlain(this, name, plan));
+        }
+    }
+
+    private void addParentNested(String name, String plan) {
+        if (parent.nested == null) {
+            parent.nested = new ArrayList<>();
+        }
+        if (!plan.isEmpty()) {
+            this.parent.nested.add(new TurboGraphPPPlanNodePlain(parent, name, plan));
+        }
     }
 
     @Property(order = 0, viewable = true)
     @Override
     public String getNodeType() {
-        return name;
+        return type = type.replace("->", "").trim();
     }
 
-    @Property(order = 1, viewable = true)
+    @Property(order = 14, viewable = false)
     @Override
     public String getNodeName() {
-        //        Object nodeName = nodeProps.get("node_name");
-        //        return nodeName == null ? null : String.valueOf(nodeName);
         return "";
     }
 
-    @Property(order = 10, viewable = true)
-    @Override
-    public Number getNodeCost() {
-        //        Object readCost = nodeProps.get("cost");
-        //        return CommonUtils.toDouble(readCost);
-        return 0;
-    }
-
-    @Override
-    public Number getNodePercent() {
-        return null;
-    }
-
-    @Override
-    public Number getNodeDuration() {
-        return null;
-    }
-
     @Property(order = 11, viewable = true)
-    @Override
-    public Number getNodeRowCount() {
-        //        Object rowCount = nodeProps.get("rows");
-        //        return rowCount == null ? null : CommonUtils.toLong(rowCount);
-        return 0;
+    public String getRow() {
+        String value = nodeProps.get("rows");
+        return value == null ? null : value;
+    }
+
+    @Property(order = 12, viewable = true)
+    public String getTime() {
+        String value = nodeProps.get("time");
+        return value == null ? null : value;
+    }
+
+    @Property(order = 13, viewable = true)
+    public String getExtra() {
+        String value = nodeProps.get("extra");
+        return value == null ? null : value;
     }
 
     @Override
@@ -139,58 +188,15 @@ public class TurboGraphPPPlanNodePlain extends AbstractExecutionPlanNode
 
     @Override
     public String toString() {
-        return object == null ? nodeProps.toString() : object.toString();
+        return plan == null ? nodeProps.toString() : plan.toString();
     }
 
-    //////////////////////////////////////////////////////////
-    // Properties
-
-    @Override
-    public Object getEditableValue() {
-        return this;
-    }
-
-    @Override
-    public DBPPropertyDescriptor[] getProperties() {
-        DBPPropertyDescriptor[] props = new DBPPropertyDescriptor[nodeProps.size()];
-        int index = 0;
-        for (Map.Entry<String, String> attr : nodeProps.entrySet()) {
-            props[index++] =
-                    new PropertyDescriptor(
-                            "Details",
-                            attr.getKey(),
-                            attr.getKey(),
-                            null,
-                            String.class,
-                            false,
-                            null,
-                            null,
-                            false);
+    private static int getBlankCount(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        int i = 0;
+        while (i < sb.length() && sb.charAt(i) == ' ') {
+            i++;
         }
-        return props;
+        return i;
     }
-
-    @Override
-    public Object getPropertyValue(@Nullable DBRProgressMonitor monitor, String id) {
-        return nodeProps.get(id.toString());
-    }
-
-    @Override
-    public boolean isPropertySet(String id) {
-        return false;
-    }
-
-    @Override
-    public boolean isPropertyResettable(String id) {
-        return false;
-    }
-
-    @Override
-    public void resetPropertyValue(@Nullable DBRProgressMonitor monitor, String id) {}
-
-    @Override
-    public void resetPropertyValueToDefault(String id) {}
-
-    @Override
-    public void setPropertyValue(@Nullable DBRProgressMonitor monitor, String id, Object value) {}
 }
