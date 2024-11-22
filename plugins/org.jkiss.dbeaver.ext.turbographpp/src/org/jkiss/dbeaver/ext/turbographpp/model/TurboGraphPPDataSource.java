0e4e0e1517f81cedb007586cd1263988199110cf
hwanyseo <hwanyseo@cubrid.com>
2024-11-22 12:53:43 +0900

    [GT-179] 4차년도 코드정리 및 안정화 http://jira.iitp.cubrid.org/browse/GT-179
    - Plan 기능 활성화

--- plugins/org.jkiss.dbeaver.ext.turbographpp/src/org/jkiss/dbeaver/ext/turbographpp/model/TurboGraphPPDataSource.java
+++ plugins/org.jkiss.dbeaver.ext.turbographpp/src/org/jkiss/dbeaver/ext/turbographpp/model/TurboGraphPPDataSource.java
@@ -26,6 +26,7 @@ import org.jkiss.dbeaver.DBException;
 import org.jkiss.dbeaver.ext.generic.model.GenericDataSource;
 import org.jkiss.dbeaver.ext.generic.model.GenericView;
 import org.jkiss.dbeaver.ext.turbographpp.model.meta.TurboGraphPPMetaModel;
+import org.jkiss.dbeaver.ext.turbographpp.model.plan.TurboGraphPPPlanAnalyser;
 import org.jkiss.dbeaver.model.DBPDataSourceContainer;
 import org.jkiss.dbeaver.model.DBPDataSourceInfo;
 import org.jkiss.dbeaver.model.DBUtils;
@@ -33,6 +34,7 @@ import org.jkiss.dbeaver.model.exec.jdbc.JDBCDatabaseMetaData;
 import org.jkiss.dbeaver.model.exec.jdbc.JDBCPreparedStatement;
 import org.jkiss.dbeaver.model.exec.jdbc.JDBCResultSet;
 import org.jkiss.dbeaver.model.exec.jdbc.JDBCSession;
+import org.jkiss.dbeaver.model.exec.plan.DBCQueryPlanner;
 import org.jkiss.dbeaver.model.impl.jdbc.JDBCUtils;
 import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
 import org.jkiss.dbeaver.model.struct.DBSObject;
@@ -147,4 +149,15 @@ public class TurboGraphPPDataSource extends GenericDataSource {
         } 
         return super.getChild(monitor,childName);
     } 
+
+    @Override
+    public <T> T getAdapter(Class<T> adapter) {
+        if (this.isTurboGraph) {
+            if (adapter == DBCQueryPlanner.class) {
+                return adapter.cast(new TurboGraphPPPlanAnalyser(this));
+            }
+        }
+        return super.getAdapter(adapter);
+    }
+
 }
